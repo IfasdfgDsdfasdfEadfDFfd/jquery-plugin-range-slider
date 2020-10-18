a21cb882ff84e6c835a5ad79df23b6b06f67cb29
gugfug <gugfug1@gmail.com>
2020-10-18 06:59:09 +0000

    fix provider

--- src/core/provider.ts
+++ src/core/provider.ts
@@ -2,11 +2,11 @@ import { Store } from "./";
 import { View } from "./view";
 
 
-export abstract class Provider<TStore, TElements extends {}> {
+export abstract class Provider<TStore, TElements> {
   readonly elements = {} as TElements;
   private _root: View|undefined;
 
-  constructor(store: Store<TStore>) {
+  constructor(store: Store<TStore, unknown>) {
     this.init(store);
     store.subscribe((state: TStore) => this.render(state));
   }
@@ -16,9 +16,9 @@ export abstract class Provider<TStore, TElements extends {}> {
   }
 
   get root(): View {
-    return this._root || new View({children: Object.values(this.elements)});
+    return this._root || new View({attrs: {}, children: Object.values(this.elements)});
   }
 
-  abstract init(store: Store<TStore>): void;
+  abstract init(store: Store<TStore, unknown>): void;
   abstract render(state: TStore): void;
 }
