b0811ffa3baaacce0d484d2be0b14b6992b818f5
gugfug <gugfug1@gmail.com>
2020-10-18 05:27:21 +0000

    test for provider

---
+++ src/core/tests/provider.test.ts
@@ -0,0 +1,26 @@
+import { Provider } from "../provider";
+import { createStore } from "../store";
+
+describe('test provider', () => {
+  test('call render every time store updated', () => {
+    const STORE = createStore('', () => {});
+    const renderHandler = jest.fn();
+
+    class SomeProvider extends Provider<{}, {}> {
+      init() {}
+      render() {
+        renderHandler();
+      }
+    }
+
+    new SomeProvider(STORE);
+
+    expect(renderHandler.mock.calls.length).toEqual(1);
+
+    STORE.dispatch({
+      type: 'type',
+    });
+
+    expect(renderHandler.mock.calls.length).toEqual(2);
+  });
+});
