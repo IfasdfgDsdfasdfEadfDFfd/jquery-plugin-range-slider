8445c1a85b38cd73cc4fd0ba7c2a5a9c9a341cb7
gugfug <gugfug1@gmail.com>
2020-10-27 10:06:27 +0000

    examples x5

--- example/index.js
+++ example/index.js
@@ -2,75 +2,85 @@ const $ = require('jquery');
 require('jquery-plugin-range-slider');
 require('./index.scss');
 
-(function (api) {
-  const el = $('#js-configuration-id-0');
+window.addEventListener('load', () => {
+  for (const index of [1, 2, 3, 4, 5]) {
+    attachConfigurationPanelToRangeSlider(index);
+  }
+});
 
-  const left = el.find('.configuration__value-left');
-  const right = el.find('.configuration__value-right');
+const attachConfigurationPanelToRangeSlider = index => {
+  (function (api) {
+    const el = $(`#js-configuration-id-${index}`);
 
-  api.subscribe(state => {
-    left.val(state.value[0]);
-    right.val(state.value[1]);
+    const left = el.find('.configuration__value-left');
+    const right = el.find('.configuration__value-right');
 
-    el.find('.configuration__min').val(state.min);
-    el.find('.configuration__max').val(state.max);
-    el.find('.configuration__step').val(state.step);
+    api.subscribe(state => {
+      left.val(state.value[0]);
+      right.val(state.value[1]);
 
-    el.find('.configuration__orient').attr('checked', state.vertical);
-    el.find('.configuration__interval-mode').attr(
-      'checked',
-      state.intervalMode,
-    );
-    el.find('.configuration__marker-visibility').attr(
-      'checked',
-      state.markerVisibility,
-    );
-    el.find('.configuration__track-scale-visibility').attr(
-      'checked',
-      state.trackScaleVisibility,
-    );
-  });
+      el.find('.configuration__min').val(state.min);
+      el.find('.configuration__max').val(state.max);
+      el.find('.configuration__step').val(state.step);
 
-  left.on('focusout', event => api.setLeftValue(parseInt(event.target.value)));
-  right.on('focusout', event =>
-    api.setRightValue(parseInt(event.target.value)),
-  );
-
-  el.find('.configuration__min').on('focusout', event =>
-    api.setMin(parseInt(event.target.value)),
-  );
-  el.find('.configuration__max').on('focusout', event =>
-    api.setMax(parseInt(event.target.value)),
-  );
-  el.find('.configuration__step').on('focusout', event =>
-    api.setStep(parseInt(event.target.value)),
-  );
-
-  let unsubscribe = () => {};
-  el.find('.configuration__prefix').on('focusout', event => {
-    unsubscribe();
-    unsubscribe = api.registerMiddleware(value => {
-      return `${event.target.value}${value}`;
+      el.find('.configuration__orient').attr('checked', state.vertical);
+      el.find('.configuration__interval-mode').attr(
+        'checked',
+        state.intervalMode,
+      );
+      el.find('.configuration__marker-visibility').attr(
+        'checked',
+        state.markerVisibility,
+      );
+      el.find('.configuration__track-scale-visibility').attr(
+        'checked',
+        state.trackScaleVisibility,
+      );
     });
-  });
 
-  el.find('.configuration__orient').on('click', event =>
-    api.setOrientVertical(!event.target.hasAttribute('checked')),
-  );
+    left.on('focusout', event =>
+      api.setLeftValue(parseInt(event.target.value)),
+    );
+    right.on('focusout', event =>
+      api.setRightValue(parseInt(event.target.value)),
+    );
 
-  el.find('.configuration__interval-mode').on('click', event =>
-    api.setIntervalMode(!event.target.hasAttribute('checked')),
-  );
+    el.find('.configuration__min').on('focusout', event =>
+      api.setMin(parseInt(event.target.value)),
+    );
+    el.find('.configuration__max').on('focusout', event =>
+      api.setMax(parseInt(event.target.value)),
+    );
+    el.find('.configuration__step').on('focusout', event =>
+      api.setStep(parseInt(event.target.value)),
+    );
 
-  el.find('.configuration__marker-visibility').on('click', event =>
-    api.setMarkerVisibility(!event.target.hasAttribute('checked')),
-  );
+    let unsubscribe = () => {};
+    el.find('.configuration__prefix').on('focusout', event => {
+      unsubscribe();
+      unsubscribe = api.registerMiddleware(value => {
+        return `${event.target.value}${value}`;
+      });
+    });
 
-  el.find('.configuration__track-scale-visibility').on('click', event =>
-    api.setTrackScaleVisibility(!event.target.hasAttribute('checked')),
+    el.find('.configuration__orient').on('click', event =>
+      api.setOrientVertical(!event.target.hasAttribute('checked')),
+    );
+
+    el.find('.configuration__interval-mode').on('click', event =>
+      api.setIntervalMode(!event.target.hasAttribute('checked')),
+    );
+
+    el.find('.configuration__marker-visibility').on('click', event =>
+      api.setMarkerVisibility(!event.target.hasAttribute('checked')),
+    );
+
+    el.find('.configuration__track-scale-visibility').on('click', event =>
+      api.setTrackScaleVisibility(!event.target.hasAttribute('checked')),
+    );
+  })(
+    $(`#js-range-slider-id-${index}`).rangeSlider({
+      value: [25, 75],
+    }),
   );
-})(
-  $('#js-range-slider-id-0').rangeSlider({
-    value: [25, 75],
-  }),
-);
+};
