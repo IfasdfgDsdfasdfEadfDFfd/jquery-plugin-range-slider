c5fb6cd75e258d8b876c4afad2cb11aaf504f079
gugfug <gugfug1@gmail.com>
2020-10-27 09:59:14 +0000

    rename configuration panel

--- example/index.js
+++ example/index.js
@@ -2,56 +2,75 @@ const $ = require('jquery');
 require('jquery-plugin-range-slider');
 require('./index.scss');
 
-
 (function (api) {
-  const el = $('#js-configurator-id-0');
+  const el = $('#js-configuration-id-0');
 
-  const left = el.find('.configurator__value-left');
-  const right = el.find('.configurator__value-right');
+  const left = el.find('.configuration__value-left');
+  const right = el.find('.configuration__value-right');
 
   api.subscribe(state => {
-
     left.val(state.value[0]);
     right.val(state.value[1]);
 
-    el.find('.configurator__min').val(state.min);
-    el.find('.configurator__max').val(state.max);
-    el.find('.configurator__step').val(state.step);
+    el.find('.configuration__min').val(state.min);
+    el.find('.configuration__max').val(state.max);
+    el.find('.configuration__step').val(state.step);
 
-    el.find('.configurator__orient').attr('checked', state.vertical);
-    el.find('.configurator__interval-mode').attr('checked', state.intervalMode);
-    el.find('.configurator__marker-visibility').attr('checked', state.markerVisibility);
-    el.find('.configurator__track-scale-visibility').attr('checked', state.trackScaleVisibility);
-
+    el.find('.configuration__orient').attr('checked', state.vertical);
+    el.find('.configuration__interval-mode').attr(
+      'checked',
+      state.intervalMode,
+    );
+    el.find('.configuration__marker-visibility').attr(
+      'checked',
+      state.markerVisibility,
+    );
+    el.find('.configuration__track-scale-visibility').attr(
+      'checked',
+      state.trackScaleVisibility,
+    );
   });
 
   left.on('focusout', event => api.setLeftValue(parseInt(event.target.value)));
-  right.on('focusout', event => api.setRightValue(parseInt(event.target.value)));
+  right.on('focusout', event =>
+    api.setRightValue(parseInt(event.target.value)),
+  );
 
-  el.find('.configurator__min').on('focusout', (event) => api.setMin(parseInt(event.target.value)));
-  el.find('.configurator__max').on('focusout', (event) => api.setMax(parseInt(event.target.value)));
-  el.find('.configurator__step').on('focusout', (event) => api.setStep(parseInt(event.target.value)));
+  el.find('.configuration__min').on('focusout', event =>
+    api.setMin(parseInt(event.target.value)),
+  );
+  el.find('.configuration__max').on('focusout', event =>
+    api.setMax(parseInt(event.target.value)),
+  );
+  el.find('.configuration__step').on('focusout', event =>
+    api.setStep(parseInt(event.target.value)),
+  );
 
   let unsubscribe = () => {};
-  el.find('.configurator__prefix').on('focusout', (event) => {
+  el.find('.configuration__prefix').on('focusout', event => {
     unsubscribe();
-    unsubscribe = api.registerMiddleware((value) => {
+    unsubscribe = api.registerMiddleware(value => {
       return `${event.target.value}${value}`;
     });
   });
 
-  el.find('.configurator__orient')
-    .on('click', (event) => api.setOrientVertical(!event.target.hasAttribute('checked')));
+  el.find('.configuration__orient').on('click', event =>
+    api.setOrientVertical(!event.target.hasAttribute('checked')),
+  );
 
-  el.find('.configurator__interval-mode')
-    .on('click', (event) => api.setIntervalMode(!event.target.hasAttribute('checked')));
+  el.find('.configuration__interval-mode').on('click', event =>
+    api.setIntervalMode(!event.target.hasAttribute('checked')),
+  );
 
-  el.find('.configurator__marker-visibility')
-    .on('click', (event) => api.setMarkerVisibility(!event.target.hasAttribute('checked')));
+  el.find('.configuration__marker-visibility').on('click', event =>
+    api.setMarkerVisibility(!event.target.hasAttribute('checked')),
+  );
 
-  el.find('.configurator__track-scale-visibility')
-    .on('click', (event) => api.setTrackScaleVisibility(!event.target.hasAttribute('checked')));
-
-})($('#js-range-slider-id-0').rangeSlider({
-  value: [25, 75],
-}));
+  el.find('.configuration__track-scale-visibility').on('click', event =>
+    api.setTrackScaleVisibility(!event.target.hasAttribute('checked')),
+  );
+})(
+  $('#js-range-slider-id-0').rangeSlider({
+    value: [25, 75],
+  }),
+);
