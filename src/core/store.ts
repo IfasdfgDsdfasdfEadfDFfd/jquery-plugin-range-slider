fee7d7363a8939a0b13c433ffc5f7bfa5e1bc698
gugfug <gugfug1@gmail.com>
2020-11-07 06:27:57 +0000

    save and load func from localStorage

--- src/core/store.ts
+++ src/core/store.ts
@@ -76,16 +76,37 @@ function createStore<TState>(
 
 function loadFromLocalStoragePlugin<T>(key: string): Plugin<T> {
   return (initState: T) => {
-    return JSON.parse(window.localStorage.getItem(key) as string) || initState;
+    let state = JSON.parse(window.localStorage.getItem(key) as string);
+
+    try {
+      Object.keys(state).forEach(key => {
+        state[key] = state[key].toString().startsWith('func:')
+          ? eval(state[key].slice(5))
+          : state[key];
+      });
+    } catch {
+      state = initState;
+    }
+
+    return state;
   };
 }
 
 function saveToLocalStoragePlugin<T>(key: string): Plugin<T> {
   return (state: T) => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-    return state;
+    const cachedState = Object.assign({}, state);
+    window.localStorage.setItem(key, stringify(state as never));
+    return cachedState;
   };
 }
+
+const stringify = (value: Record<string, unknown>): string => {
+  Object.keys(value).forEach(key => {
+    value[key] =
+      typeof value[key] === 'function' ? 'func:' + value[key] : value[key];
+  });
+  return JSON.stringify(value);
+};
 
 function NaNValidator(action: Action): Action {
   if (typeof action.value === 'number') {
