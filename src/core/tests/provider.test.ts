0bcfc146c43a280b430ca1f01a474227da87a209
gugfug <gugfug1@gmail.com>
2021-01-18 11:58:29 +1000

    update provider tests

--- src/core/tests/provider.test.ts
+++ src/core/tests/provider.test.ts
@@ -1,14 +1,20 @@
+import { View } from '..';
 import { Provider } from '../provider';
-import { createStore } from '../store';
+import { createStore, Store } from '../store';
 
 describe('test provider', () => {
+  let STORE: Store<string>;
+  beforeEach(() => {
+    STORE = createStore<string>('', () => '');
+  });
+
   test('call render every time store updated', () => {
-    const STORE = createStore<string>('', () => '');
     const renderHandler = jest.fn();
+    const initHandler = jest.fn();
 
     class SomeProvider extends Provider<string, unknown> {
       init() {
-        // placeholder
+        initHandler();
       }
       render() {
         renderHandler();
@@ -18,6 +24,7 @@ describe('test provider', () => {
     new SomeProvider(STORE);
 
     expect(renderHandler.mock.calls.length).toEqual(1);
+    expect(initHandler.mock.calls.length).toEqual(1);
 
     STORE.dispatch({
       name: 'type',
@@ -25,5 +32,17 @@ describe('test provider', () => {
     });
 
     expect(renderHandler.mock.calls.length).toEqual(2);
+    expect(initHandler.mock.calls.length).toEqual(1);
+  });
+
+  test('init root View if it was not provided explicitly', () => {
+    class SomeProvider extends Provider<string, unknown> {
+      init() {}
+      render() {}
+    }
+    const provider = new SomeProvider(STORE);
+
+    expect(provider.root).toBeInstanceOf(View);
+    expect(provider.root.element).toBeInstanceOf(HTMLDivElement);
   });
 });
