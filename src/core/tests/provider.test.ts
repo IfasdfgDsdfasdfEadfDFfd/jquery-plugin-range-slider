ff3dd23f99b28403e7275dc66ec2de08dd46647b
gugfug <gugfug1@gmail.com>
2021-01-26 21:40:35 +1000

    remove unnecessary test

--- src/core/tests/provider.test.ts
+++ src/core/tests/provider.test.ts
@@ -6,27 +6,6 @@ describe('test provider', () => {
     STORE = createStore<string>('', action => action.value);
   });
 
-  test('call render every time store updated', () => {
-    const initHandler = jest.fn();
-
-    class SomeProvider extends Provider<string, unknown> {
-      init() {
-        initHandler();
-      }
-    }
-
-    new SomeProvider(STORE);
-
-    expect(initHandler.mock.calls.length).toEqual(1);
-
-    STORE.dispatch({
-      name: 'type',
-      value: '',
-    });
-
-    expect(initHandler.mock.calls.length).toEqual(1);
-  });
-
   test('init root View if it was not provided explicitly', () => {
     class SomeProvider extends Provider<string, unknown> {
       init() {}
