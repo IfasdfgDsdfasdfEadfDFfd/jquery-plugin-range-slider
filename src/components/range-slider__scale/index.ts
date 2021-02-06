32356bb83742c2f3a3f08c3008e95f691cd89c1f
gugfug <gugfug1@gmail.com>
2021-02-06 21:23:59 +1000

    create scale component

--- src/components/range-slider__scale/index.ts
+++ src/components/range-slider__scale/index.ts
@@ -1,3 +1,4 @@
 export * from './range-slider__scale.view';
 export * from './range-slider__scale.model';
 export * from './range-slider__scale.controller';
+export * from './range-slider__scale.component';
