60d244d189e32a12226047ef598ec7b89090d820
gugfug <gugfug1@gmail.com>
2021-02-06 21:25:30 +1000

    empty file

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { TrackComponent } from './components/range-slider__track';
-
-const trackComponent = new TrackComponent();
-trackComponent.view.init();
-trackComponent.attachToDocument(document.body);
