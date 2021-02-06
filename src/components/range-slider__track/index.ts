d9743e1b3f8cf731e90649bfd7632c4f0b965295
gugfug <gugfug1@gmail.com>
2021-02-06 21:22:42 +1000

    update exports

--- src/components/range-slider__track/index.ts
+++ src/components/range-slider__track/index.ts
@@ -1,2 +1,4 @@
 export * from './range-slider__track.view';
 export * from './range-slider__track.model';
+export * from './range-slider__track.controller';
+export * from './range-slider__track.component';
